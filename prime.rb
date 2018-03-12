3670c870ea4f47f2458350da3c0e9f75c44fe869
Denise Pendleton <dpendl1@hotmail.com>
2018-03-12 23:06:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(n)
 
   (3..n).each do
-     (n % 2) == 0 && (n % 3) == 0 && (n % 5) == 0 && (n % 7) == 0
+     if (n % 2) == 0 && (n % 3) == 0 && (n % 5) == 0 && (n % 7) == 0
       return false
       d += 1
     else
