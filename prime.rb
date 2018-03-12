b4d6e9d836284314b45374efd80e4d95940a686f
Denise Pendleton <dpendl1@hotmail.com>
2018-03-12 22:59:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 def prime?(n)
-  for d in 2..(n - 1)
-     if (n % d) == 0
+  d = 2
+  (2..n).each do 
+     (n % d) == 0
       return false
+      d += 1
     else
       true
      end
