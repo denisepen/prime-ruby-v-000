4bae1404585029566fe113bf458e359497a29b14
Denise Pendleton <dpendl1@hotmail.com>
2018-03-12 23:11:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(n)
 
-     if (n % 2) == 0 && (n % 3) == 0 && (n % 5) == 0 && (n % 7) == 0 && n < = 0
+     if (n % 2) == 0 && (n % 3) == 0 && (n % 5) == 0 && (n % 7) == 0 && n =< 0
     false
     else
       true
